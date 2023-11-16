0606ede0f4f11fb37993e51e713344c871e71403
Cauchy <caotricg1308@gmail.com>
2023-11-16 12:56:30 +0700

    update exercise 4

--- 02-File_System/exer4/main.c
+++ 02-File_System/exer4/main.c
@@ -6,5 +6,20 @@
 
 int main()
 {
-    
+    int fd, wr;
+    char buff[12] = "test APPEND";
+
+    fd = open("text.txt",O_RDWR | O_APPEND, 0777);
+    if (fd == -1)
+    {
+        printf("open() file text.txt failed\n");
+    }
+
+    lseek(fd, 0, SEEK_SET);
+
+    wr = write(fd, buff, strlen(buff));
+    if (wr == -1)
+    {
+        printf("write() file text.txt failed\n");
+    }
 }
